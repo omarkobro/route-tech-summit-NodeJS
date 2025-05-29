2fb84a63a426dac8d6dba81d8981f5e992b45f9f
Omar Kobro <omakobro567@gmail.com>
2025-05-29 20:09:20 +0300

    Pushing Untracked Changes

--- src/middlewares/globalResponse.js
+++ src/middlewares/globalResponse.js
@@ -2,7 +2,7 @@ export const globalResponse =  (err, req, res, next) => {
     if (err) {
         return res.status(err['cause'] || 500).json({
             message: 'Catch error',
-            error_msg: err.message,
+            error_msg: err.message
         })
     }
 }
